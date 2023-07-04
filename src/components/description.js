8e52d016edc84ed1c0457d4af0a47c238f43a209
Rachel Isaac <rachelisaac995@gmail.com>
2023-07-04 04:23:20 -0400

    Add styles to the description component

--- src/components/description.js
+++ src/components/description.js
@@ -9,38 +9,44 @@ const Description = () => (
       <img
         src={mobileIllustration}
         alt="mobile illustration"
+        className="illustration-img"
       />
-      <h1 className="desc-header">Stay updated!</h1>
-      <p className="descr-text">
-        Join 60,000+ product managers receiving monthly updates on:
-      </p>
-      <ul className="update-list">
-        <li>
-          <img
-            src={icon}
-            alt="icon check"
-          />
-          <p className="update-desc">
-            Product discovery and building what matters
-          </p>
-        </li>
-        <li>
-          <img
-            src={icon}
-            alt="icon check"
-          />
-          <p className="update-desc">
-            Measuring to ensure updates are a success
-          </p>
-        </li>
-        <li>
-          <img
-            src={icon}
-            alt="icon check"
-          />
-          <p className="update-desc">And much more!</p>
-        </li>
-      </ul>
+      <div className="text-container">
+        <h1 className="desc-header">Stay updated!</h1>
+        <p className="descr-text">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+        <ul className="update-list">
+          <li>
+            <img
+              src={icon}
+              alt="icon check"
+              className="icon-check"
+            />
+            <p className="update-desc">
+              Product discovery and building what matters
+            </p>
+          </li>
+          <li>
+            <img
+              src={icon}
+              alt="icon check"
+              className="icon-check"
+            />
+            <p className="update-desc">
+              Measuring to ensure updates are a success
+            </p>
+          </li>
+          <li>
+            <img
+              src={icon}
+              alt="icon check"
+              className="icon-check"
+            />
+            <p className="update-desc">And much more!</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </>
 );
