0995b7d5de05cec130791bafc68371bdd34c7a0c
Rachel Isaac <rachelisaac995@gmail.com>
2023-07-07 04:19:37 -0400

    Add form component and style it

--- src/components/form.js
+++ src/components/form.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/form.css';
 
-const Form = () => (
-  <Form className="form-container">
-    <p className="email-label">Email address</p>
-    <input
-      className="email-input"
-      type="text"
-      id="email"
-      placeholder="email@company.com"
-    />
-    <button type="submit">Subscribe to monthly newsletter</button>
-  </Form>
-);
+const Form = () => {
+  const [email, setEmail] = useState('');
+
+  const handleFormSubmit = (event) => {
+    // to prevent a full page reload on submission
+
+    event.preventDefault();
+
+    setEmail('');
+  };
+
+  return (
+    <form
+      className="form-container"
+      onSubmit={handleFormSubmit}
+    >
+      <p className="email-label">Email address</p>
+      <input
+        className="email-input"
+        type="text"
+        id="email"
+        placeholder="email@company.com"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <button
+        type="submit"
+        className="btn-form"
+      >
+        Subscribe to monthly newsletter
+      </button>
+    </form>
+  );
+};
 
 export default Form;
